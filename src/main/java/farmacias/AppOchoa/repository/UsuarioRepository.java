3351e1d140e194cadaa2aafb8343551096a39539
julixxx23 <jorellanao12@miumg.edu.gt>
2025-11-24 12:13:12 -0600

    feat: desarollo de servicios

--- src/main/java/farmacias/AppOchoa/repository/UsuarioRepository.java
+++ src/main/java/farmacias/AppOchoa/repository/UsuarioRepository.java
@@ -3,5 +3,17 @@ package farmacias.AppOchoa.repository;
 import farmacias.AppOchoa.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    // Verifica si ya existe un usuario con ese nombre de usuario
+    boolean existsByNombreUsuarioUsuario(String nombreUsuarioUsuario);
+
+    // Busca un usuario por su nombre de usuario (para login, por ejemplo)
+    Optional<Usuario> findByNombreUsuarioUsuario(String nombreUsuarioUsuario);
+
+    // Lista todos los usuarios que tengan usuarioEstado = true
+    List<Usuario> findByUsuarioEstadoTrue();
 }
