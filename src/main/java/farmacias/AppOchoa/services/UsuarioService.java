9dda9585681002926b39b346eedf506a559f02d1
julixxx23 <j4608448@gmail.com>
2025-11-24 12:13:12 -0600

    feat: desarollo de servicios

---
+++ src/main/java/farmacias/AppOchoa/services/UsuarioService.java
@@ -0,0 +1,20 @@
+package farmacias.AppOchoa.services;
+
+import farmacias.AppOchoa.dto.usuario.LoginDTO;
+import farmacias.AppOchoa.dto.usuario.UsuarioCreateDTO;
+import farmacias.AppOchoa.dto.usuario.UsuarioUpdateDTO;
+import farmacias.AppOchoa.dto.usuario.UsuarioResponseDTO;
+import farmacias.AppOchoa.dto.usuario.UsuarioSimpleDTO;
+
+import java.util.List;
+
+public interface UsuarioService {
+
+    UsuarioResponseDTO crearUsuario(UsuarioCreateDTO dto);
+    UsuarioResponseDTO actualizarUsuario(Long id, UsuarioUpdateDTO dto);
+    void eliminarUsuario(Long id);
+    void cambiarEstado(Long id, Boolean nuevoEstado);
+    UsuarioResponseDTO obtenerPorId(Long id);
+    List<UsuarioSimpleDTO> listarUsuariosActivos();
+    UsuarioResponseDTO login(LoginDTO dto);
+}
