935db48bcfb9c18e08d08f531b4f45c4b2c0e3b1
julixxx23 <j4608448@gmail.com>
2025-12-18 23:03:57 -0600

    Feat: Implementacion de Controllers en Sucursal, Presentacion

--- src/main/java/farmacias/AppOchoa/services/SucursalService.java
+++ src/main/java/farmacias/AppOchoa/services/SucursalService.java
@@ -4,23 +4,21 @@ import farmacias.AppOchoa.dto.sucursal.SucursalCreateDTO;
 import farmacias.AppOchoa.dto.sucursal.SucursalResponseDTO;
 import farmacias.AppOchoa.dto.sucursal.SucursalSimpleDTO;
 import farmacias.AppOchoa.dto.sucursal.SucursalUpdateDTO;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface SucursalService {
 
+    // Métodos de escritura
     SucursalResponseDTO crear(SucursalCreateDTO dto);
-
-    SucursalResponseDTO obtenerPorId (Long id);
-    List<SucursalSimpleDTO> listarTodas();
-    List<SucursalSimpleDTO> listarActivas();
-
     SucursalResponseDTO actualizar(Long id, SucursalUpdateDTO dto);
     void cambiarEstado(Long id, Boolean estado);
+    void eliminar(Long id);
 
-    void eliminarSucursal(Long id);
+    // Métodos de consulta
+    SucursalResponseDTO obtenerPorId(Long id);
 
-
-
-
+    // Métodos de paginación
+    Page<SucursalSimpleDTO> listarActivasPaginadas(Pageable pageable);
+    Page<SucursalSimpleDTO> listarTodasPaginadas(Pageable pageable);
 }
