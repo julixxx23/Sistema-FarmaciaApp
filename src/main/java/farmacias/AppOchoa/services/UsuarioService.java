6132b8a1cccf596c3679582b9449d43a08b28f23
julixxx23 <j4608448@gmail.com>
2025-12-20 19:18:45 -0600

    Refact: Refactorizacion de services y repositoriesn con paginacion

--- src/main/java/farmacias/AppOchoa/services/UsuarioService.java
+++ src/main/java/farmacias/AppOchoa/services/UsuarioService.java
@@ -5,18 +5,19 @@ import farmacias.AppOchoa.dto.usuario.UsuarioCreateDTO;
 import farmacias.AppOchoa.dto.usuario.UsuarioUpdateDTO;
 import farmacias.AppOchoa.dto.usuario.UsuarioResponseDTO;
 import farmacias.AppOchoa.dto.usuario.UsuarioSimpleDTO;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UsuarioService {
 
     UsuarioResponseDTO crearUsuario(UsuarioCreateDTO dto);
-    UsuarioResponseDTO actualizarUsuario(Long id, UsuarioUpdateDTO dto);
-
-    void eliminarUsuario(Long id);
-    void cambiarEstado(Long id, Boolean nuevoEstado);
 
     UsuarioResponseDTO obtenerPorId(Long id);
-    List<UsuarioSimpleDTO> listarUsuariosActivos();
+    Page<UsuarioSimpleDTO> listarUsuariosActivosPaginado(Pageable pageable);
+
+    UsuarioResponseDTO actualizarUsuario(Long id, UsuarioUpdateDTO dto);
+    void cambiarEstado(Long id, Boolean nuevoEstado);
+    void eliminarUsuario(Long id);
+
     UsuarioResponseDTO login(LoginDTO dto);
 }
