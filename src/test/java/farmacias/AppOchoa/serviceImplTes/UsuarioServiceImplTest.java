1b47e3b1b8fb698b63cef68c4d8977dad161b7c1
julixxx23 <jorellanao12@miumg.edu.gt>
2026-01-22 22:42:36 -0600

    Implement Test Login

---
+++ src/test/java/farmacias/AppOchoa/serviceImplTes/UsuarioServiceImplTest.java
@@ -0,0 +1,25 @@
+package farmacias.AppOchoa.serviceImplTes;
+
+import farmacias.AppOchoa.repository.SucursalRepository;
+import farmacias.AppOchoa.serviceimpl.UsuarioServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UsuarioServiceImplTest {
+    @Mock
+    private SucursalRepository sucursalRepository;
+    @InjectMocks
+    private UsuarioServiceImpl usuarioService;
+
+    //TEST PARA CREAR USUARIO
+    @Test
+    @DisplayName("Deberia crear un usuario cuando los datos son validos")
+    void crearUsuario_Exito(){
+        return;
+    }
+}
