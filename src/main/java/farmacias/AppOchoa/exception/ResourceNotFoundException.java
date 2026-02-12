0dabe514b5182a54c1a68e1f83110262e7e7a25f
julixxx23 <jorellanao12@miumg.edu.gt>
2026-02-11 22:57:10 -0600

    feat: add BadRequestException for business logic validation

--- src/main/java/farmacias/AppOchoa/exception/ResourceNotFoundException.java
+++ src/main/java/farmacias/AppOchoa/exception/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package farmacias.AppOchoa.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.FOUND)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends  RuntimeException {
     public ResourceNotFoundException(String mensaje){
         super(mensaje); // mensaje de la excepcion
