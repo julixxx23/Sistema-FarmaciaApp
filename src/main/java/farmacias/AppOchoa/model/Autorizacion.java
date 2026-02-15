6a3af0f8cdc2aa79b3eb65b5ec1ece07c97cf7ab
julixxx23 <jorellanao12@miumg.edu.gt>
2026-02-15 15:42:07 -0600

    feat(model): add Caja, CajaSesiones and CajaCorte entities

---
+++ src/main/java/farmacias/AppOchoa/model/Autorizacion.java
@@ -0,0 +1,15 @@
+package farmacias.AppOchoa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "autorizacion")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Autorizacion {
+}
