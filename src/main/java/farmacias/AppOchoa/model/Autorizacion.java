de20d5a963d2f54b95677c07cc97b7510b3c9a46
julixxx23 <jorellanao12@miumg.edu.gt>
2026-02-15 16:04:24 -0600

    feat(model): add Autorizacion  entities

--- src/main/java/farmacias/AppOchoa/model/Autorizacion.java
+++ src/main/java/farmacias/AppOchoa/model/Autorizacion.java
@@ -1,15 +1,42 @@
 package farmacias.AppOchoa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "autorizacion")
+@Table(name = "autorizaciones")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
-public class Autorizacion {
+public class Autorizacion{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long autorizacionId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "autorizacion_tipo", nullable = false)
+    private AutorizacionTipo autorizacionTipo;
+
+    @CreationTimestamp
+    @Column(name = "autorizacion_fecha", nullable = false, updatable = false)
+    private LocalDateTime autorizacionFecha;
+
+    @Column(name = "autorizacion_referencia_id", nullable = false)
+    private Long autorizacionReferenciaId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_cajero_id")
+    private Usuario cajero;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_supervisor_id")
+    private Usuario supervisor;
+
+
+
 }
