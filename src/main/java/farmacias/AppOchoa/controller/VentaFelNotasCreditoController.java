689760ee6ffa1926a839aa1c5cd68e298fa9feb8
julixxx23 <jorellanao12@miumg.edu.gt>
2026-03-02 22:45:59 -0600

    refactor(api): clean up VentaFelNotasCredito routes
    - Consolidate GET / and GET /buscar endpoints.
    - Update ID search endpoint to standard GET /{id}.

--- src/main/java/farmacias/AppOchoa/controller/VentaFelNotasCreditoController.java
+++ src/main/java/farmacias/AppOchoa/controller/VentaFelNotasCreditoController.java
@@ -1,11 +1,16 @@
 package farmacias.AppOchoa.controller;
 
+import farmacias.AppOchoa.dto.ventafelnotascredito.VentaFelNotasCreditoCreateDTO;
+import farmacias.AppOchoa.dto.ventafelnotascredito.VentaFelNotasCreditoResponseDTO;
 import farmacias.AppOchoa.dto.ventafelnotascredito.VentaFelNotasCreditoSimpleDTO;
 import farmacias.AppOchoa.services.VentaFelNotasCreditoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,4 +29,25 @@ public class VentaFelNotasCreditoController {
             Pageable pageable) {
         return ResponseEntity.ok(ventaFelNotasCreditoService.buscarPorTexto(texto, pageable));
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<VentaFelNotasCreditoResponseDTO> buscarPorId(@PathVariable Long id){
+        VentaFelNotasCreditoResponseDTO ventaFelNotasCreditoResponseDTO = ventaFelNotasCreditoService.buscarPorId(id);
+        return ResponseEntity.ok(ventaFelNotasCreditoResponseDTO);
+    }
+    @GetMapping
+    public ResponseEntity<Page<VentaFelNotasCreditoSimpleDTO>> listarNotas(
+            @PageableDefault(size = 10, sort = "notaEstado")Pageable pageable){
+        return ResponseEntity.ok(ventaFelNotasCreditoService.listarNotas(pageable));
+    }
+    @PostMapping
+    public ResponseEntity<VentaFelNotasCreditoResponseDTO> crear(@Valid @RequestBody VentaFelNotasCreditoCreateDTO dto){
+        VentaFelNotasCreditoResponseDTO ventaFelNotasCreditoResponseDTO = ventaFelNotasCreditoService.crear(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ventaFelNotasCreditoResponseDTO);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+        ventaFelNotasCreditoService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
