bd814ce08baf932b83db4abfa67fa1db6c60c829
julixxx23 <jorellanao12@miumg.edu.gt>
2026-03-06 23:55:35 -0600

    feat: add SuscripcionPagos repository

---
+++ AppOchoa/src/main/java/farmacias/AppOchoa/repository/SuscripcionPagoRepository.java
@@ -0,0 +1,7 @@
+package farmacias.AppOchoa.repository;
+
+import farmacias.AppOchoa.model.SuscripcionPago;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface SuscripcionPagoRepository extends JpaRepository<SuscripcionPago, Long> {
+
+}
