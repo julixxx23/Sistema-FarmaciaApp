cf63e2710cd9ea0f0fccaa1567586578e5ee1aa6
julixxx23 <jorellanao12@miumg.edu.gt>
2026-03-12 22:29:27 -0600

    feat: add FarmaciaService and SuscripcionPagoService interfaces

--- AppOchoa/src/main/java/farmacias/AppOchoa/services/FarmaciaService.java
+++ AppOchoa/src/main/java/farmacias/AppOchoa/services/FarmaciaService.java
@@ -8,10 +8,11 @@ import farmacias.AppOchoa.repository.FarmaciaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface FarmaciaService extends FarmaciaRepository {
-    FarmaciaResponseDTO crear (Long farmaciaId,FarmaciaCreateDTO dto);
-    FarmaciaResponseDTO buscarId(Long farmaciaId, Long id);
-    Page<FarmaciaSimpleDTO> listarFarmacias(Long farmaciaId, Pageable pageable);
-    void eliminar(Long farmaciaId, Long id);
+public interface FarmaciaService {
+    FarmaciaResponseDTO crear (FarmaciaCreateDTO dto);
+    FarmaciaResponseDTO buscarId(Long id);
+    Page<FarmaciaSimpleDTO> listarFarmacias(Pageable pageable);
+    void eliminar(Long id);
+    Page<FarmaciaSimpleDTO> buscarPorTexto(String texto, Pageable pageable);
 
 }
