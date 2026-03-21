59f0f12034411c8f5eaf1a1c355951cbf1e3d3d5
julixxx23 <jorellanao12@miumg.edu.gt>
2026-03-20 23:38:23 -0600

    configuration CrossOrigin

--- src/main/java/farmacias/AppOchoa/controller/AlertaController.java
+++ src/main/java/farmacias/AppOchoa/controller/AlertaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/alertas")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*") // URL Despegada
 public class AlertaController {
     private final AlertaService alertaService;
     private final JwtUtil jwtUtil;
